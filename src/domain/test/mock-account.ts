aeeb6c268b6e4e35207cb5f3121bd69a80dde47f
Samuel <samuelbrehm@gmail.com>
2021-02-05 11:26:38 -0300

    feat: ensure Authentication returns as Account oon success

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,7 +1,13 @@
+import { AccountModel } from '@/domain/models/account-model'
 import { AuthenticationParams } from '@/domain/usecases/authentication'
+
 import faker from 'faker'
 
 export const mockAuthentication = (): AuthenticationParams => ({
   email: faker.internet.email(),
   password: faker.internet.password()
+})
+
+export const mockAccountModel = (): AccountModel => ({
+  accessToken: faker.random.uuid()
 })
