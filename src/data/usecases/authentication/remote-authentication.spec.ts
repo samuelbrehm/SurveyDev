29de8c640e393b122a2b5558af30c41627f00ca7
Samuel <samuelbrehm@gmail.com>
2021-02-05 11:40:03 -0300

    refactor: group imports in index files

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,12 +1,11 @@
 import { RemoteAuthentication } from '@/data/usecases/authentication/remote-authentication'
-import { HttpPostClientSpy } from '@/data/test/mock-http-client'
-import { HttpStatusCode } from '@/data/protocols/http/http-response'
+import { HttpPostClientSpy } from '@/data/test'
+import { HttpStatusCode } from '@/data/protocols/http'
 
-import { AccountModel } from '@/domain/models/account-model'
+import { AccountModel } from '@/domain/models'
 import { AuthenticationParams } from '@/domain/usecases/authentication'
-import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
-import { mockAuthentication } from '@/domain/test/mock-authentication'
-import { UnexpectedError } from '@/domain/errors/unexpected-error'
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
+import { mockAccountModel, mockAuthentication } from '@/domain/test'
 
 import faker from 'faker'
 
@@ -73,5 +72,16 @@ describe('RemoteAuthentication', () => {
     }
     const promise = sut.auth(mockAuthentication())
     await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  test('Should return as AccountModel if HttpPostClient returns 200', async () => {
+    const httpResult = mockAccountModel()
+    const { httpPostClientSpy, sut } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: httpResult
+    }
+    const account = await sut.auth(mockAuthentication())
+    expect(account).toEqual(httpResult)
   })
 })
--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,27 +1,25 @@
-import { HttpPostClient } from '@/data/protocols/http/http-post-client'
-import { HttpStatusCode } from '@/data/protocols/http/http-response'
+import { HttpPostClient, HttpStatusCode } from '@/data/protocols/http'
 
-import { AccountModel } from '@/domain/models/account-model'
-import { AuthenticationParams } from '@/domain/usecases/authentication'
-import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
-import { UnexpectedError } from '@/domain/errors/unexpected-error'
+import { AccountModel } from '@/domain/models'
+import { Authentication, AuthenticationParams } from '@/domain/usecases'
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
   constructor (
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>
   ) {
   }
 
-  async auth (params: AuthenticationParams): Promise<void> {
-    const httpPostClient = await this.httpPostClient.post({
+  async auth (params: AuthenticationParams): Promise<AccountModel> {
+    const httpResponse = await this.httpPostClient.post({
       url: this.url,
       body: params
     })
 
-    switch (httpPostClient.statusCode) {
+    switch (httpResponse.statusCode) {
       case HttpStatusCode.ok:
-        break
+        return httpResponse.body
       case HttpStatusCode.unauthorized:
         throw new InvalidCredentialsError()
       default:
