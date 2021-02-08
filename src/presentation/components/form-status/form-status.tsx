bf678350b994313279b52c8237efb0cf092b4515
Samuel <samuelbrehm@gmail.com>
2021-02-08 11:55:57 -0300

    feat: ensure Login will not load spinner and error on start

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Styles from './form-status-styles.scss'
 import { Spinner } from '@/presentation/components'
+import Context from '@/presentation/contexts/form/form-context'
 
-const FormStatus: React.FC = () => (
-  <div className={Styles.errorWrapper}>
-    <Spinner className={Styles.spinner} />
-    <span className={Styles.error}>Erro</span>
-  </div>
-)
+const FormStatus: React.FC = () => {
+  const { isLoading, errorMessage } = useContext(Context)
+
+  return (
+    <div data-testid="error-wrap" className={Styles.errorWrapper}>
+      {isLoading && <Spinner className={Styles.spinner}/>}
+      {errorMessage && <span className={Styles.error}>{errorMessage}</span>}
+    </div>
+  )
+}
 
 export default FormStatus
