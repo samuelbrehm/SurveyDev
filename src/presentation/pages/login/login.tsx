4f2fce956a418294d60112d5b79ee1fcf56e3a49
Samuel <samuelbrehm@gmail.com>
2021-02-08 09:16:09 -0300

    refactor: add index components folder to export

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,23 +1,21 @@
 import React from 'react'
 import Styles from './login-styles.scss'
-import LoginHeader from '@/presentation/components/login-header/login-header'
-import Footer from '@/presentation/components/footer/footer'
-import Input from '@/presentation/components/input/intpu'
-import FormStatus from '@/presentation/components/form-status/form-status'
+import { LoginHeader } from '@/presentation/components/'
+import { Footer, FormStatus, Input } from '@/presentation/components'
 
 const Login: React.FC = () => {
   return (
     <div className={Styles.login}>
-      <LoginHeader />
+      <LoginHeader/>
       <form className={Styles.form}>
         <h2>Login</h2>
-        <Input type="email" name="email" placeholder="Digite seu e-mail" />
-        <Input type="password" name="password" placeholder="Digite sua senha" />
+        <Input type="email" name="email" placeholder="Digite seu e-mail"/>
+        <Input type="password" name="password" placeholder="Digite sua senha"/>
         <button className={Styles.submit} type="submit">Entrar</button>
         <span className={Styles.link}>Criar conta</span>
-        <FormStatus />
+        <FormStatus/>
       </form>
-      <Footer />
+      <Footer/>
     </div>
 
   )
