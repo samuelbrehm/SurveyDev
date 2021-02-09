2a2729babcedc033086ca9fe17df25be86aee638
Samuel <samuelbrehm@gmail.com>
2021-02-09 08:10:57 -0300

    feat: ensure Login present spinner on submit

--- src/presentation/components/spinner/spinner.tsx
+++ src/presentation/components/spinner/spinner.tsx
@@ -4,7 +4,7 @@ import Styles from './spinner-styles.scss'
 type Props = React.HTMLAttributes<HTMLElement>
 
 const Spinner: React.FC<Props> = (props: Props) => (
-  <div className={[Styles.spinner, props.className].join(' ')}>
+  <div {...props} data-testid="spinner" className={[Styles.spinner, props.className].join(' ')}>
     <div/>
     <div/>
     <div/>
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -28,11 +28,16 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
     })
   }, [state.email, state.password])
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault()
+    setState({ ...state, isLoading: true })
+  }
+
   return (
     <div className={Styles.login}>
       <LoginHeader/>
       <Context.Provider value={{ state, setState }}>
-        <form className={Styles.form}>
+        <form className={Styles.form} onSubmit={handleSubmit}>
           <h2>Login</h2>
           <Input type="email" name="email" placeholder="Digite seu e-mail"/>
           <Input type="password" name="password" placeholder="Digite sua senha"/>
@@ -41,7 +46,7 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
             disabled={!!state.emailError || !!state.passwordError}
             className={Styles.submit}
             type="submit">
-              Entrar
+            Entrar
           </button>
           <span className={Styles.link}>Criar conta</span>
           <FormStatus/>
