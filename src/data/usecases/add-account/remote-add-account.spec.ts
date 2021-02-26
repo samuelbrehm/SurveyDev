8476836fce80868ba96c5ec3e88fd8061072c224
Samuel <samuelbrehm@gmail.com>
2021-02-26 13:58:51 -0300

    test: ensure RemoteAddAccount throws EmailInUseError if HttpPostClient return 403

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -2,10 +2,12 @@ import faker from 'faker'
 
 import { RemoteAddAccount } from './remote-add-account'
 import { HttpPostClientSpy } from '@/data/test'
+import { HttpStatusCode } from '@/data/protocols/http'
 
 import { AccountModel } from '@/domain/models'
 import { AddAccountParams } from '@/domain/usecases'
-import { mockAddAccountParams, mockAuthentication } from '@/domain/test'
+import { mockAddAccountParams } from '@/domain/test'
+import { EmailInUseError } from '@/domain/errors'
 
 type SutTypes = {
   sut: RemoteAddAccount
@@ -34,5 +36,14 @@ describe('RemoteAddAccount', () => {
     const addAccountParams = mockAddAccountParams()
     await sut.add(addAccountParams)
     expect(httpPostClientSpy.body).toEqual(addAccountParams)
+  })
+
+  test('Should throw EmailInUseError if HttpPostClient returns 403', async () => {
+    const { httpPostClientSpy, sut } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden
+    }
+    const promise = sut.add(mockAddAccountParams())
+    await expect(promise).rejects.toThrow(new EmailInUseError())
   })
 })
