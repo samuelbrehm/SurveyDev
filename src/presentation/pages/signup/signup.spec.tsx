8200298e91a4b7c140eb6f3471cc9256dc702f46
Samuel <samuelbrehm@gmail.com>
2021-03-01 11:47:45 -0300

    test: ensure SignUp enable submit button if form is valid

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -98,4 +98,13 @@ describe('Signup Component', () => {
     Helper.populateField(sut, 'passwordConfirmation')
     Helper.testStatusForField(sut, 'passwordConfirmation')
   })
+
+  test('Should enable submit button if form is valid', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'name')
+    Helper.populateField(sut, 'email')
+    Helper.populateField(sut, 'password')
+    Helper.populateField(sut, 'passwordConfirmation')
+    Helper.testButtonIsDisable(sut, 'submit', false)
+  })
 })
